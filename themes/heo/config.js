6b8637122edce5e4702355be1b09db0719a4a955
fenghao <fh@fhcy.vip>
2025-02-22 11:36:47 +0800

    调整

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,17 +19,17 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: 'FHCY.TOP',
+  HEO_HERO_TITLE_4: '学无止境',
+  HEO_HERO_TITLE_5: '代码可以重构，但人生无法回滚。',
+  HEO_HERO_TITLE_LINK: 'https://fhcy.top',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'Java', url: '/tag/Java' },
+  HEO_HERO_CATEGORY_2: { title: '生活日常', url: '/tag/日常' },
+  HEO_HERO_CATEGORY_3: { title: '更多标签', url: '/tag' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -58,60 +58,60 @@ const CONFIG = {
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
-      color_2: '#ffffff'
-    },
-    {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-      color_2: '#4082c3'
-    },
-    {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      title_1: 'CSS',
+      img_1: '/images/heo/CSS.png',
       color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      title_2: 'docker',
+      img_2: '/images/heo/docker.png',
       color_2: '#ffffff'
     },
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
+      title_1: 'html',
+      img_1: '/images/heo/html.png',
+      color_1: '#ffffff',
+      title_2: 'kafka',
+      img_2: '/images/heo/kafka.png',
+      color_2: '#ffffff'
     },
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
-      color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
-      color_2: '#2c51db'
+      title_1: 'linux',
+      img_1: '/images/heo/linux.png',
+      color_1: '#ffffff',
+      title_2: 'mysql',
+      img_2: '/images/heo/mysql.png',
+      color_2: '#ffffff'
     },
     {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
-      color_2: '#e9572b'
+      title_1: 'nginx',
+      img_1: '/images/heo/nginx.png',
+      color_1: '#ffffff',
+      title_2: 'notion',
+      img_2: '/images/heo/notion.png',
+      color_2: '#ffffff'
     },
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
-      color_2: '#1f1f1f'
+      title_1: 'python',
+      img_1: '/images/heo/python.png',
+      color_1: '#ffffff',
+      title_2: 'rabbitmq',
+      img_2: '/images/heo/rabbitmq.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Redis',
+      img_1: '/images/heo/Redis.png',
+      color_1: '#ffffff',
+      title_2: 'shell',
+      img_2: '/images/heo/shell.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'ubuntu',
+      img_1: '/images/heo/ubuntu.png',
+      color_1: '#ffffff',
+      title_2: 'shell',
+      img_2: '/images/heo/shell.png',
+      color_2: '#ffffff'
     }
   ],
 
