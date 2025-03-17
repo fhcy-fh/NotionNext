f6d52f0a5fe2a918cb5a3e74e867e6bbc3aad24c
fenghao <fh@fhcy.vip>
2025-03-17 17:18:20 +0800

    照片优化

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -6,14 +6,6 @@ import { siteConfig } from '@/lib/config'
  */
 export default function PoweredBy(props) {
   return (
-    <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
-      <a
-        href='https://github.com/fhcy-fh/NotionNext'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
-      .
-    </div>
+    <div></div>
   )
 }
